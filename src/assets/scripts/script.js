7911cc530b671c12c15626626058c2654cae42ad
Maksim Danilau <dannyloumax@gmail.com>
2024-01-19 17:54:42 +0300

    fix(hw-11): fix styles and html tag

---
+++ src/assets/scripts/script.js
@@ -0,0 +1,41 @@
+// Получаем ссылку на элемент
+let scrollingElement = document.querySelector('header__container');
+      
+// Функция, которая будет вызываться при прокрутке
+function handleScroll() {
+  // Проверяем, насколько прокручена страница по вертикали
+  var scrollPosition = window.scrollY;
+
+  // Условие, при котором добавляем класс
+  if (scrollPosition > 0) {
+    scrollingElement.classList.add('scrolled');
+  } else {
+    scrollingElement.classList.remove('scrolled');
+  }
+}
+
+// Добавляем прослушиватель события прокрутки
+window.addEventListener('scroll', handleScroll);
+
+// Получаем ссылки на элементы
+let menuButton = document.querySelector('.header__dropdown');
+let menu = document.querySelector('.header__nav');
+
+// Добавляем слушатели событий
+menuButton.addEventListener('click', toggleMenu);
+
+// Функция для открытия/закрытия меню
+function toggleMenu() {
+    menu.classList.toggle('open');
+}
+
+// Добавляем слушатель события для каждого пункта меню
+var menuItems = document.querySelectorAll('.header__link');
+menuItems.forEach(function(item) {
+    item.addEventListener('click', closeMenu);
+});
+
+// Функция для закрытия меню
+function closeMenu() {
+    menu.classList.remove('open');
+}
